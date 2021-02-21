3f8e4248dbfc5ccc77a2da6ba9814b361abc7d98
madchurro <maddiechernosky1@gmail.com>
2021-02-20 18:07:59 -0700

    Update LifetimeHeatCalc.js

--- LifetimeHeatCalc.js
+++ LifetimeHeatCalc.js
@@ -1,7 +1,3 @@
-// Lifetime Heats Calculator
-
-//Initialize
-
 function calculateLifetime() {
   var year2 = Number(document.getElementById("year2").value);
   var month2 = Number(document.getElementById("month2").value);
@@ -10,21 +6,6 @@ function calculateLifetime() {
   var totalheats = 0;
   var iterate = true;
 
-  // Conditionals
-
-  if (year2 == 7 && month2 > 2) {
-    document.getElementById("LifetimeHeatsCalculation").innerHTML = "Female wolves cannot be bred past the age of 7 years and 2 months.";
-  } else if (year2 >= 8) {
-    document.getElementById("LifetimeHeatsCalculation").innerHTML = "Wolves cannot live past the age of 8 years.";
-  } else if (year2 < 1) {
-    document.getElementById("LifetimeHeatsCalculation").innerHTML = "Wolves cannot breed until they are 1 year old.";
-  } else if (month2 > 12 || month2 < 0) {
-    document.getElementById("LifetimeHeatsCalculation").innerHTML = "Months must be between 0 and 12.";
-  } else if (cooldown2 > 20 || cooldown2 < 0) {
-    document.getElementById("LifetimeHeatsCalculation").innerHTML = "Cooldown must be between 1 and 20.";
-  }
-
-  // Calculation
   else {
     while (iterate == true) {
       month2 + cooldown2;
@@ -50,3 +31,15 @@ function calculateLifetime() {
     }
   }
 }
+
+if (year2 == 7 && month2 > 2) {
+    document.getElementById("LifetimeHeatsCalculation").innerHTML = "Female wolves cannot be bred past the age of 7 years and 2 months.";
+  } else if (year2 >= 8) {
+    document.getElementById("LifetimeHeatsCalculation").innerHTML = "Wolves cannot live past the age of 8 years.";
+  } else if (year2 < 1) {
+    document.getElementById("LifetimeHeatsCalculation").innerHTML = "Wolves cannot breed until they are 1 year old.";
+  } else if (month2 > 12 || month2 < 0) {
+    document.getElementById("LifetimeHeatsCalculation").innerHTML = "Months must be between 0 and 12.";
+  } else if (cooldown2 > 20 || cooldown2 < 0) {
+    document.getElementById("LifetimeHeatsCalculation").innerHTML = "Cooldown must be between 1 and 20.";
+  }
